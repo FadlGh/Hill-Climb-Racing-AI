6f72d7f50cf0a0476aca26a604579393fed37b38
FadlGh <97055147+FadlGh@users.noreply.github.com>
2023-01-16 20:30:24 +0200

    Box Spawning 1

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -19,7 +19,8 @@ public class TerrainGenerator : MonoBehaviour
 
     [Range(0, 500)]
     public int numOfPoints;
-    
+
+    public GameObject Boxes;
     // Start is called before the first frame update
     void Start()
     {
@@ -38,6 +39,12 @@ public class TerrainGenerator : MonoBehaviour
                 randomNumber = Random.Range(shape.spline.GetPosition(2).y, shape.spline.GetPosition(2).y + 2);
             }
             shape.spline.InsertPointAt(i + 2, new Vector3(xPos, height * Mathf.PerlinNoise(0, i * randomNumber)));
+
+            if(i == 40)
+            {
+                Vector3 Pos = shape.spline.GetPosition(i) + new Vector3(0, 10, 0);
+                Instantiate(Boxes, Pos, Quaternion.identity);
+            }
         }
 
         for (int i = 2; i <= numOfPoints + 2; i++)
